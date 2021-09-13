a694f744ac82473e4c910030eaf2c5a1c4cec249
Devon Ankar <devon.ankar@gmail.com>
2021-09-12 22:58:47 -0400

    move comment into readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # County-level analysis of GDP and 2020 election results
+
+## GDP
+
+Via [BEA - GDP by County, Metro, and Other Areas](https://www.bea.gov/data/gdp/gdp-county-metro-and-other-areas). From [this page](https://apps.bea.gov/regional/downloadzip.cfm), I downloaded the dataset "CAGDP1: GDP Summary by County and MSA". County data was used in this analysis, and GDP is in current-dollar GDP (thousands of current dollars).
+
+## Population
+
+Population estimates from [this page](https://www.census.gov/data/tables/time-series/demo/popest/2010s-counties-total.html): "Datasets" > "Population, Population Change, and Estimated Components of Population Change: April 1, 2010 to July 1, 2019 (CO-EST2019-alldata)"
+
+## State FIPS
+
+FIPS code for each state from [this link at census.gov](https://www.census.gov/geographies/reference-files/2017/demo/popest/2017-fips.html)
+
+## Election Results
+
+Unofficial county-level election results via Politico / Associated Press feed. (I originally did this analysis before official results were available.)
+
+Scraped for each state FIPS code using the following URL formats:
+
+- counties (FIPS-to-name): https://www.politico.com/2020-statewide-metadata/{STATE_FIPS_CODE}/county-names.meta.json
+- candidates: https://www.politico.com/2020-statewide-metadata/{STATE_FIPS_CODE}/potus.meta.json
+- county-level results: https://www.politico.com/2020-statewide-results/{STATE_FIPS_CODE}/potus-counties.json
+
+---
+
+## To-Do
+
+- use official election results instead
+- publish Tableau Public workbook/visualizations
--- task.py
+++ task.py
@@ -1,31 +1,3 @@
-"""
-
-# Data Sources
-
-## GDP
-
-Via [BEA - GDP by County, Metro, and Other Areas](https://www.bea.gov/data/gdp/gdp-county-metro-and-other-areas). From [this page](https://apps.bea.gov/regional/downloadzip.cfm), I downloaded the dataset "CAGDP1: GDP Summary by County and MSA". County data was used in this analysis, and GDP is in current-dollar GDP (thousands of current dollars).
-
-## Population
-
-Population estimates from [this page](https://www.census.gov/data/tables/time-series/demo/popest/2010s-counties-total.html): "Datasets" > "Population, Population Change, and Estimated Components of Population Change: April 1, 2010 to July 1, 2019 (CO-EST2019-alldata)"
-
-## State FIPS
-
-FIPS code for each state from [this link at census.gov](https://www.census.gov/geographies/reference-files/2017/demo/popest/2017-fips.html)
-
-## Election Results
-
-Unofficial county-level election results via Politico / Associated Press feed. (I originally did this analysis before the official results were available.)
-
-Scraped for each state FIPS code using the following URL formats:
-
-- counties (FIPS-to-name): https://www.politico.com/2020-statewide-metadata/{STATE_FIPS_CODE}/county-names.meta.json
-- candidates: https://www.politico.com/2020-statewide-metadata/{STATE_FIPS_CODE}/potus.meta.json
-- county-level results: https://www.politico.com/2020-statewide-results/{STATE_FIPS_CODE}/potus-counties.json
-
-"""
-
 from json import dump
 from time import sleep
 import numpy as np
@@ -45,8 +17,6 @@ class FilenamePrefixes:
 
 
 class SingleStateScraper:
-    # if I were doing this again, I would download the raw files and then parse them separately
-
     _counties_url = 'https://www.politico.com/2020-statewide-metadata/{}/county-names.meta.json'
     _candidates_url = 'https://www.politico.com/2020-statewide-metadata/{}/potus.meta.json'
     _results_url = 'https://www.politico.com/2020-statewide-results/{}/potus-counties.json'
